55d8d2419d41476ec31a6ded49afbcfa051e46cc
jennyfromtheweb <jennyzhcl@yahoo.com>
2015-09-29 18:01:38 -0700

    wrote translate_reg function

--- src/translate_utils.c
+++ src/translate_utils.c
@@ -58,7 +58,11 @@ int translate_num(long int* output, const char* str, long int lower_bound,
     if (!str || !output) {
         return -1;
     }
-    /* YOUR CODE HERE */
+    long int temp = strtol(str, NULL, 0);
+    if (temp != 0 && temp >= lower_bound && temp <= upper_bound) {
+        *output = temp;
+        return 0;
+    }
     return -1;
 }
 
@@ -73,6 +77,18 @@ int translate_reg(const char* str) {
     else if (strcmp(str, "$at") == 0)   return 1;
     else if (strcmp(str, "$v0") == 0)   return 2;
     else if (strcmp(str, "$a0") == 0)   return 4;
-    /* YOUR CODE HERE */
+    else if (strcmp(str, "$a1") == 0)   return 5;
+    else if (strcmp(str, "$a2") == 0)   return 6;
+    else if (strcmp(str, "$a3") == 0)   return 7;
+    else if (strcmp(str, "$t0") == 0)   return 8;
+    else if (strcmp(str, "$t1") == 0)   return 9;
+    else if (strcmp(str, "$t2") == 0)   return 10;
+    else if (strcmp(str, "$t3") == 0)   return 11;
+    else if (strcmp(str, "$s0") == 0)   return 16;
+    else if (strcmp(str, "$s1") == 0)   return 17;
+    else if (strcmp(str, "$s2") == 0)   return 18;
+    else if (strcmp(str, "$s3") == 0)   return 19;
+    else if (strcmp(str, "$sp") == 0)   return 29;
+    else if (strcmp(str, "$ra") == 0)   return 31;
     else                                return -1;
 }
