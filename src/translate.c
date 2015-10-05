217dfcfec93300f7dd31c061172d45976b523252
Alice Tarng <atarng@airbears2-10-142-65-34.AirBears2.1918.Berkeley.EDU>
2015-10-04 21:53:52 -0700

    translate.c clean

--- src/translate.c
+++ src/translate.c
@@ -53,25 +53,23 @@ int check_num_args(int num_args, int correct_num_args) {
 
 int li_can_rep_32 (char** args) {
     // Range from min value of signed int to max value of unsigned ints
-    // Immediate should be 2nd arg.
-    // **args here, apparently, is an array of strings. 
-    // Array of char*s = array of strings.
     char* end;  
     long int imm = strtol(args[1], &end, 0);
+
     if (imm >= INT32_MIN && imm <= UINT32_MAX) {
         return 1;
     }
     return 0; 
 }
 
-/* Should return # instructions written. */
+/* Return # instructions written. */
 int li_expansion(char** args, FILE* output) {
-    // Check if can fit in imm field of an addiu instruction; optimization.
-    //sdfsdfsdfchar* end; 
-    //long int imm = strtol(args[1], &end, 0);
+    // Check if can fit in imm field of an addiu instruction
     int failure;
     long int imm_pointer; 
+
     failure = translate_num(&imm_pointer, args[1], INT32_MIN, UINT32_MAX);
+
     if (failure) {
       return 0; 
     }
@@ -79,50 +77,30 @@ int li_expansion(char** args, FILE* output) {
     int num_instruct = 0;  
     if ((imm_pointer) >= INT16_MIN && (imm_pointer) <= UINT16_MAX) {
       // Make addiu instruction. 
-      // addiu $dest $base value 
-      // failure = write_addiu(9, output, args, num_args);
       fprintf(output, "addiu %s, $0, %li\n", args[0], imm_pointer);
       num_instruct++;
 
-      // if (!failure) {
-      //   num_instruct++;
-      // }
     } else {
-
       // Otherwise expand into lui-ori.
-      // lui $at, num
-      // ori $at, $at, immediate
-      // addu $dest, $dest, $at
-
-      //failure = write_lui(15, output, args, num_args);
-      // Want first 16 bits = logical shift 
+      // Want first 16 bits
       int lui_int = (imm_pointer) >> 16;
-      uint32_t mask = 0x0000ffff; // for removing leading     1's
+      uint32_t mask = 0x0000ffff; 
+      // For removing leading 1's
       lui_int &= mask; 
       fprintf(output, "lui $at, %i\n", lui_int);
       num_instruct++;
 
-      // Okay should be fine; shifting won't change the original value. 
-      //failure = write_ori(13, output, args, num_args);
       int ori_int = ((imm_pointer) << 16) >> 16;
-      //uint32_t mask = 0x0000ffff; // for removing leading 1's
       ori_int &= mask; 
       fprintf(output, "ori %s, $at, %i\n", args[0], ori_int);
       num_instruct++;
-      // if (!failure) {
-      //     num_instruct++;
-      // }
     }
+
     return num_instruct; 
-    
 }
 
 
 int move_expansion(char** args, FILE* output) {
-    // Move is like basically just addu. 
-    // args: first is dest, 2nd is what is being copied
-    // addiu args[0], args[1], $0; 
-    //int failure;
     int num_instruct = 0;
     fprintf(output, "addiu %s, %s, $0\n", args[0], args[1]);
     num_instruct++;
@@ -133,47 +111,25 @@ int move_expansion(char** args, FILE* output) {
 int blt_expansion(char** args, FILE* output) {
     // Want to use slt to get 1
     // And then use bne with zero
-    //int failure;
     int num_instruct = 0;
-    // Slt $at, args[0], args[1]
-    // char** slt_args;
-    // slt_args[0] = "$at";
-    // slt_args[1] = args[0];
-    // slt_args[2] = args[1];
-    // failure = write_rtype(42, output, slt_args, 3);
-    // if (!failure) {
-    //   num_instruct++;
-    // }
-
     fprintf(output, "slt $at, %s, %s\n", args[0], args[1]);
     num_instruct++;
 
-    // slt would be 1 if indeed less than. Otherwise 0.
+    // slt 1 if less than; otherwise 0.
 
-    // Now bne
-    // bne $at, $0, args[2]
-    // char** bne_args;
-    // bne_args[0] = "$at";
-    // bne_args[1] = "$0";
-    // bne_args[2] = args[2];
     fprintf(output, "bne $at, $0, %s\n", args[2]);
     num_instruct++;
 
-    return num_instruct; 
-    // PC + 4. BUT HOW GET PC OMG
-    //failure = write_branch(5, output, bne_args, 3, )
+    return num_instruct;
 }
+
 
 int bgt_expansion(char** args, FILE* output) {
     int num_instruct = 0;
-    // Same as blt I think, just flip the registers when slt.
-    // Just to make sure = case doesn't get caught up. 
-
-    // slt $at, args[1], args[0]
+    // Same as blt, just flip the registers when slt.
     fprintf(output, "slt $at, %s, %s\n", args[1], args[0]);
     num_instruct++;
 
-    // bne $at, $0, args[2]
     fprintf(output, "bne $at, $0, %s\n", args[2]);;
     num_instruct++;
 
@@ -182,86 +138,69 @@ int bgt_expansion(char** args, FILE* output) {
 
 int traddu_expansion(char** args, FILE* output) {
      int num_instruct = 0; 
-     // Traddu is just a triple add instead of a double add. 
-     // I'll just addu the first 2 and put into $at, then addu $at with the last one and put into final dest. 
-     // addu $at, args[0], args[1]
      fprintf(output, "addu $at, %s, %s\n", args[0], args[1]);
      num_instruct++;
 
-     // addu args[0], $at, args[2]
      fprintf(output, "addu %s, $at, %s", args[0], args[2]);
      num_instruct++; 
 
      return num_instruct; 
 }
 
-int swpr_expansion(char** args, FILE* output) {
-    int num_instruct = 0;
 
-    // Swaps the values. 
-    // Save one value to $at, then just add. 
+int swpr_expansion(char** args, FILE* output) {
+    int num_instruct = 0; 
 
     // Save 1st value in $at. 
     // addu $at, arg[0], $0
     fprintf(output, "addu $at, %s, $0\n", args[0]);
     num_instruct++;
 
-    // Now move 2nd to first.
-    // addu arg[0], arg[1], $0
+    // Move 2nd to first.
     fprintf(output, "addu %s, %s, $0\n", args[0], args[1]);
     num_instruct++;
 
-    // Now move 1st value (stored in $at) to 2nd. 
-    // addu args[1], $at, $0
+    // Move 1st value (stored in $at) to 2nd. 
     fprintf(output, "addu %s, $at, $0\n", args[1]);
     num_instruct++;
 
     return num_instruct;
 }
 
+
 int mul_expansion(char** args, FILE* output) {
     int num_instruct = 0;
 
     // Wants the lower 32 bits of the product - $lo. 
-
-    // Becomes mult and mflo
-    // Mult - hilo get the full 64 bits. Hi = upper, lo = lower. 
-    // So if want the lower 32, just grab lo. 
-
-    // mult args[1], args[2]
     fprintf(output, "mult %s, %s\n", args[1], args[2]);
     num_instruct++;
 
-    // Wanted is in $lo now - move it
-    // mflo args[0]
     fprintf(output, "mflo %s\n", args[0]);
     num_instruct++;
 
     return num_instruct; 
 }
+
 
 int div_expansion(char** args, FILE* output) {
     int num_instruct = 0;
 
     // Return quotient - in lo. 
-    // div args[1] args[2]
     fprintf(output, "div %s, %s\n", args[1], args[2]);
     num_instruct++;
-    // mflo args[0]
     fprintf(output, "mflo %s\n", args[0]);
     num_instruct++;
 
     return num_instruct; 
 }
 
+
 int rem_expansion(char** args, FILE* output) {
     int num_instruct = 0;
 
-    // It's just like div, but grab from hi instead. 
-    // div args[1] args[2]
+    // Like div, but grab hi instead. 
     fprintf(output, "div %s, %s\n", args[1], args[2]);
     num_instruct++;
-    // mfhi args[0]
     fprintf(output, "mfhi %s\n", args[0]);
     num_instruct++;
 
@@ -271,61 +210,48 @@ int rem_expansion(char** args, FILE* output) {
 
 
 
-
-
 unsigned write_pass_one(FILE* output, const char* name, char** args, int num_args) {
     if (strcmp(name, "li") == 0) {
-        /* YOUR CODE HERE */
-        // li should take in 2 args. 
         if (check_num_args(num_args, 2) && li_can_rep_32(args)) {
             return li_expansion(args, output);
         }
-        return 0; // For very primal bad errors. 
+        return 0; 
     } else if (strcmp(name, "move") == 0) {
-        /* YOUR CODE HERE */
-        // move should take in 2 args
         if (check_num_args(num_args, 2)) {
             return move_expansion(args, output);
         }
         return 0;  
     } else if (strcmp(name, "blt") == 0) {
-        /* YOUR CODE HERE */
         if (check_num_args(num_args, 3)) {
             return blt_expansion(args, output);
         }
         return 0;  
     } else if (strcmp(name, "bgt") == 0) {
-        /* YOUR CODE HERE */
         if (check_num_args(num_args, 3)) {
             return bgt_expansion(args, output);
         }
         return 0;  
     } else if (strcmp(name, "traddu") == 0) {
-        /* YOUR CODE HERE */
         if (check_num_args(num_args, 3)) {
             return traddu_expansion(args, output);
         }
         return 0;       
     } else if (strcmp(name, "swpr") == 0) {
-        /* YOUR CODE HERE */
         if (check_num_args(num_args, 2)) {
             return swpr_expansion(args, output);
         }
         return 0;       
     } else if (strcmp(name, "mul") == 0) {
-        /* YOUR CODE HERE */
         if (check_num_args(num_args, 3)) {
             return mul_expansion(args, output);
         }
         return 0;       
     } else if (strcmp(name, "div") == 0) {
-        /* YOUR CODE HERE */
         if (check_num_args(num_args, 3)) {
             return div_expansion(args, output);
         }
         return 0;       
     } else if (strcmp(name, "rem") == 0) {
-        /* YOUR CODE HERE */
         if (check_num_args(num_args, 3)) {
             return rem_expansion(args, output);
         }
@@ -361,6 +287,7 @@ unsigned write_pass_one(FILE* output, const char* name, char** args, int num_arg
  */
 int translate_inst(FILE* output, const char* name, char** args, size_t num_args, uint32_t addr,
     SymbolTable* symtbl, SymbolTable* reltbl) {
+
     if (strcmp(name, "addu") == 0)       return write_rtype (0x21, output, args, num_args);
     else if (strcmp(name, "or") == 0)    return write_rtype (0x25, output, args, num_args);
     else if (strcmp(name, "slt") == 0)   return write_rtype (0x2a, output, args, num_args);
@@ -394,7 +321,7 @@ int translate_inst(FILE* output, const char* name, char** args, size_t num_args,
    find bitwise operations to be the cleanest way to complete this function.
  */
 int write_rtype(uint8_t funct, FILE* output, char** args, size_t num_args) {
-    // Perhaps perform some error checking?
+
     if (num_args != 3) {
         return -1;
     }
@@ -426,7 +353,6 @@ int write_rtype(uint8_t funct, FILE* output, char** args, size_t num_args) {
    find bitwise operations to be the cleanest way to complete this function.
  */
 int write_shift(uint8_t funct, FILE* output, char** args, size_t num_args) {
-    // Perhaps perform some error checking?
     if (num_args != 3) {
         return -1;
     }
@@ -452,7 +378,6 @@ int write_shift(uint8_t funct, FILE* output, char** args, size_t num_args) {
 /* The rest of your write_*() functions below */
 
 int write_jr(uint8_t funct, FILE* output, char** args, size_t num_args) {
-    // Perhaps perform some error checking?
     if (num_args != 1) {
         return -1;
     }
@@ -469,19 +394,18 @@ int write_jr(uint8_t funct, FILE* output, char** args, size_t num_args) {
 }
 
 int write_addiu(uint8_t opcode, FILE* output, char** args, size_t num_args) {
-    // Perhaps perform some error checking?
     if (num_args != 3) {
         return -1;
     }    
     long int imm;
     int rt = translate_reg(args[0]);
     int rs = translate_reg(args[1]);
-    // need clarification on addiu - what does it mean to pass in a negative immediate?
     int err = translate_num(&imm, args[2], INT16_MIN, INT16_MAX);
     if (err != -1 && rs != -1 && rt != -1) {
         uint32_t instruction = 0;
         // ORDER: opcode, rs, rt, imm
-        uint32_t mask = 0x0000ffff; // for removing leading 1's
+        // For removing leading 1's
+        uint32_t mask = 0x0000ffff; 
         imm &= mask;
         instruction += imm;
         instruction += (rt << 16);
@@ -494,7 +418,6 @@ int write_addiu(uint8_t opcode, FILE* output, char** args, size_t num_args) {
 }
 
 int write_ori(uint8_t opcode, FILE* output, char** args, size_t num_args) {
-    // Perhaps perform some error checking?
     if (num_args != 3) {
         return -1;
     }
@@ -516,7 +439,6 @@ int write_ori(uint8_t opcode, FILE* output, char** args, size_t num_args) {
 }
 
 int write_lui(uint8_t opcode, FILE* output, char** args, size_t num_args) {
-    // Perhaps perform some error checking?
     if (num_args != 2) {
         return -1;
     }
@@ -537,7 +459,6 @@ int write_lui(uint8_t opcode, FILE* output, char** args, size_t num_args) {
 }
 
 int write_mem(uint8_t opcode, FILE* output, char** args, size_t num_args) {
-    // Perhaps perform some error checking?
     if (num_args != 3) {
         return -1;
     }
@@ -549,7 +470,7 @@ int write_mem(uint8_t opcode, FILE* output, char** args, size_t num_args) {
     if (err != -1 && rs != -1 && rt != -1) {
         uint32_t instruction = 0;
         // ORDER: opcode, rs, rt, imm
-        uint32_t mask = 0x0000ffff; // for removing leading 1's
+        uint32_t mask = 0x0000ffff; 
         imm &= mask;
         instruction += imm;
         instruction += (rt << 16);
@@ -571,16 +492,15 @@ static int can_branch_to(uint32_t src_addr, uint32_t dest_addr) {
 
 
 int write_branch(uint8_t opcode, FILE* output, char** args, size_t num_args, uint32_t addr, SymbolTable* symtbl) {
-    // Perhaps perform some error checking?
+
     if (num_args != 3) {
         return -1;
     }
     int rs = translate_reg(args[0]);
     int rt = translate_reg(args[1]);
     int label_addr = get_addr_for_symbol(symtbl, args[2]);
-    // Need to check if can_branch_to and if offset fits inside the immediate field
-    // assuming that addr is the PC address
     // Check if branch offset fits inside immediate field
+    // Also check if can branch. 
     if (label_addr == -1 || rs == -1 || rt == -1 || !can_branch_to(addr, label_addr) || ((label_addr << 16) >> 16 != label_addr)) {
         return -1;
     }
@@ -600,13 +520,7 @@ int write_jump(uint8_t opcode, FILE* output, char** args, size_t num_args, uint3
     if (num_args != 1) {
         return -1;
     }
-    int label_addr;
-    // Need absolute address of the label to jump to
-    //label_addr = get_addr_for_symbol(reltbl, args[0]);
-    //if (label_addr == -1) {
-        // if no address is found, set the address to 0
-        label_addr = 0;
-    //}
+    int label_addr = 0;
     add_to_table(reltbl, args[0], addr);
     uint32_t instruction = 0;
     // ORDER: opcode, addr
